56c261d34a6796a7783e57e5abd3f7a3538e0840
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-06-18 19:44:23 -0500

    Added sums to html, need to wire

---
+++ scripts/client.js
@@ -0,0 +1,19 @@
+console.log('js');
+
+$(document).ready(onReady);
+
+function onReady() {
+  console.log('DOM ready');
+}
+
+
+
+
+  /*  one invisible row 
+  .toggleClass(invisible)
+       <tr>
+      <td><div class="block invisible" id="redBlock"></div></td>
+      <td><div class="block invisible" id="blueBlock"></div></td>
+      <td><div class="block invisible" id="greenBlock"></div></td>
+      <td><div class="block invisible" id="yellowBlock"></div></td>
+    </tr>*/
