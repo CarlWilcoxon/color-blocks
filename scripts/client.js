5b292b24a30127473e671226dce12c8ab708ae7b
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-06-19 10:40:50 -0500

    can toggle blocks and count them

--- scripts/client.js
+++ scripts/client.js
@@ -1,19 +1,79 @@
 console.log('js');
 
+let redCount = 0;
+let blueCount = 0;
+let greenCount = 0;
+let yellowCount = 0;
+
 $(document).ready(onReady);
 
 function onReady() {
   console.log('DOM ready');
+  $('button').on('click', addBlock);
+  $('table').on('click', '.block', toggleBlock);
+  
 }
 
+function addBlock () {
+  console.log(this);
+  
+  $('table').last('tr').append(`<tr>
+  <td><div class="block invisible red"></div></td>
+      <td><div class="block invisible blue"></div></td>
+      <td><div class="block invisible green"></div></td>
+      <td><div class="block invisible yellow"></div></td>
+    </tr>`);
+  
+  
+  if ($(this).is('#btnRedAdd')) {
+    console.log('this is the red button');
+    findNext();
+  }
+  
+  updateColorCount();
+}
 
+function findNext() {
+  
+}
+function toggleBlock() {
+  console.log(this);
+  if ($(this).hasClass('red')) {
+    if ($(this).hasClass('invisible')) {
+      redCount++;
+    } else {
+      redCount--;
+    }
+  } else if ($(this).hasClass('blue')) {
+    if ($(this).hasClass('invisible')) {
+      blueCount++;
+    } else {
+      blueCount--;
+    }
+  } else if ($(this).hasClass('green')) {
+    if ($(this).hasClass('invisible')) {
+      greenCount++;
+    } else {
+      greenCount--;
+    }
+  } else if ($(this).hasClass('yellow')) {
+    if ($(this).hasClass('invisible')) {
+      yellowCount++;
+    } else {
+      yellowCount--;
+    }
+  }
+  $(this).toggleClass('invisible');
+  updateColorCount();
+}
 
-
-  /*  one invisible row 
-  .toggleClass(invisible)
-       <tr>
-      <td><div class="block invisible" id="redBlock"></div></td>
-      <td><div class="block invisible" id="blueBlock"></div></td>
-      <td><div class="block invisible" id="greenBlock"></div></td>
-      <td><div class="block invisible" id="yellowBlock"></div></td>
-    </tr>*/
+function updateColorCount() {
+  $('#redSum').empty();
+  $('#redSum').append(redCount);
+  $('#blueSum').empty();
+  $('#blueSum').append(blueCount);
+  $('#greenSum').empty();
+  $('#greenSum').append(greenCount);
+  $('#yellowSum').empty();
+  $('#yellowSum').append(yellowCount);
+}
